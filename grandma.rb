a03dfebd3c62cb9fe8261f1c8c539edc05a2a48e
Nathan Rathe <nprathe10@gmail.com>
2019-05-25 00:30:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ if phrase == phrase.upcase
   then return "NO, NOT SINCE 1938!"
 elsif phrase == "I LOVE YOU GRANDMA!"
   then return "I LOVE YOU TOO PUMPKIN!"
-else return "HUH?! SPEAK UP, SUNNY!"
+else return "HUH?! SPEAK UP, SONNY!"
 end
 
 
