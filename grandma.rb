85f4d9ec6c0f4a1e3025d112e00dbab427ba7d3b
Nathan Rathe <nprathe10@gmail.com>
2019-05-25 00:47:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,9 @@
 def speak_to_grandma(string)
 
 if string == "I LOVE YOU GRANDMA!"
-  then return "I LOVE YOU TOO PUMPKIN!"
+  return "I LOVE YOU TOO PUMPKIN!"
 if string == string.upcase
-  then return "NO, NOT SINCE 1938!"
+  return "NO, NOT SINCE 1938!"
 else return "HUH?! SPEAK UP, SONNY!"
   end
 end
